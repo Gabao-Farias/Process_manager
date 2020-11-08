db364432526e82a5710f8136dc149611b4ec8d53
Gabao-Farias <gabrielfariasbass@gmail.com>
2020-11-08 17:27:57 -0300

    Added dashboard view on main

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,25 @@
-import React from 'react';
-import { PagesTable, ProcessTable } from './components';
+import React, { useState } from 'react';
+import { DashboardWrapper, PagesTable, ProcessTable } from './components';
 import Header from './components/Header';
 import PagesTableGenerator from './utils/PagesTableGenerator';
 
 
 function App() {
+  const [tablePageConfig, setTablePageConfig] = useState(PagesTableGenerator(0));
 
   return (
-    <div className="App" style={{backgroundColor: "#333333"}}>
-      <Header/>
-      <PagesTable tablePageConfig={PagesTableGenerator(16)}/>
-      <ProcessTable process={{
-        pid: "2",
-        processSize: 256,
-        fragmentSize: 50
-      }} />
+    <div className="App" style={{backgroundColor: "#333333", minHeight: "100vh"}}>
+      <Header defineTamp={() => setTablePageConfig} />
+      <DashboardWrapper
+        left={<PagesTable tablePageConfig={tablePageConfig}/>}
+        right={
+          <ProcessTable process={{
+            pid: "28252",
+            processSize: 256,
+            fragmentSize: 50
+          }} />
+        }
+      />
     </div>
   );
 }
