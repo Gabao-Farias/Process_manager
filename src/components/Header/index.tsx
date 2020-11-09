b80d65b0aab8d12e0300257b5aad4c359a2dad9b
Gabao-Farias <gabrielfariasbass@gmail.com>
2020-11-08 22:09:57 -0300

    Changed handling OnChange

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,18 +2,18 @@ import React, { FC, useState } from 'react';
 import { HeaderContainer, HeaderOption, HeaderSmallForm, HeaderInput } from './styles';
 
 interface HeaderProps {
-  defineTamp: () => {};
+  defineTamp: (tamp: number) => {};
 }
 
 const Header : FC<HeaderProps> = ({defineTamp}) => {
-  const [tamp, setTamp] = useState();
+  const [tamp, setTamp] = useState(16);
 
   return(
     <HeaderContainer>
       <HeaderSmallForm>
         <strong>1º</strong>
-        <HeaderInput value={tamp} type="number" placeholder="Insira o tamanho da página em Bytes" />
-        <HeaderOption onClick={() => {}}>Definir tamanho da página</HeaderOption>
+        <HeaderInput value={tamp} onChange={(e) => {setTamp(Number(e.target.value))}} placeholder="Insira o tamanho da página em Bytes" />
+        <HeaderOption onClick={() => {defineTamp(tamp)}}>Definir tamanho da página</HeaderOption>
       </HeaderSmallForm>
       <HeaderSmallForm>
         <strong>2º</strong>
