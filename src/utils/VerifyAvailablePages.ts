0f5583312ab845699aae8a011d999bf13425852f
Gabao-Farias <gabrielfariasbass@gmail.com>
2020-11-10 07:45:26 -0300

    Fixed ESLint unused expression

--- src/utils/VerifyAvailablePages.ts
+++ src/utils/VerifyAvailablePages.ts
@@ -4,9 +4,11 @@ const VerifyAvailablePages = (pageTable : PageTable[]) : Number => {
   var availablePages = 0;
   const objetoDoTipoPageTable = pageTable;
 
-  for (let i = 0; i < objetoDoTipoPageTable.length; i++)
-    objetoDoTipoPageTable[i].used ? null : availablePages++;
-
+  for (let i = 0; i < objetoDoTipoPageTable.length; i++){
+    if(objetoDoTipoPageTable[i].used === false){
+      availablePages++;
+    }
+  }
   return(availablePages);
 }
 
