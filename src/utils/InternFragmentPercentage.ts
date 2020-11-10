7a21b9e334ddeebd68136c88e62e759edc8986f5
Gabao-Farias <gabrielfariasbass@gmail.com>
2020-11-10 07:49:28 -0300

    Added to round number

--- src/utils/InternFragmentPercentage.ts
+++ src/utils/InternFragmentPercentage.ts
@@ -1,7 +1,7 @@
 const InternFragmentPercentage = (tamp: number, usedPages: number, processSize: number) => {
   var full = (usedPages * tamp);
   var part = (usedPages * tamp) - processSize;
-  return (part * 100 / full);
+  return (Math.round(part * 100 / full));
 }
 
 export default InternFragmentPercentage;
