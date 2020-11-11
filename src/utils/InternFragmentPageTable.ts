1bfb7c93a3cc9b6b5cde611285085d5c49c1ac67
Gabao-Farias <gabrielfariasbass@gmail.com>
2020-11-11 09:17:34 -0300

    Added intern fragment of pages table feature

---
+++ src/utils/InternFragmentPageTable.ts
@@ -0,0 +1,14 @@
+import { Process } from "../Models";
+import InternFragment from "./InternFragment";
+
+const InternFragmentPageTable = (processes : Process[], pageSize : number) => {
+  var i = 0;
+  var totalFragment : number = 0;
+  while(i < processes.length){
+    totalFragment += InternFragment(pageSize, processes[i].busyPages, processes[i].processSize);
+    i++;
+  }
+  return totalFragment;
+}
+
+export default InternFragmentPageTable;
---
+++ src/utils/InternFragmentPageTablePercentage.ts
@@ -0,0 +1,11 @@
+import { InternFragmentPageTable } from ".";
+import { Process } from "../Models";
+
+const InternFragmentPageTablePercentage = (processes : Process[], pageSize : number) => {
+  const availableMemory = 8 * 1024;
+  var totalFragment : number = InternFragmentPageTable(processes, pageSize);
+  
+  return((totalFragment * 100) / availableMemory);
+}
+
+export default InternFragmentPageTablePercentage;
